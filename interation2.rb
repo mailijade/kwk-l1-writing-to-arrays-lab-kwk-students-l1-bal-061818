c2787dbe64839e5c5e870bad64c36826db3e1470
mailijade <mailijade23@gmail.com>
2018-06-20 19:54:57 +0000

    automatically backed up by learn

--- interation2.rb
+++ interation2.rb
@@ -1,12 +1,3 @@
-# price_with_tax = [ ]
-
-# cart_item_price = [12.50,19.99,3.49,7.99]
-# cart_item_price.each do |x|
-#     new_price = x*1.17
-#     price_with_tax << new_price
-# end
-# puts price_with_tax
-
 total= 7.50
 cart_item_price = [12.50,19.99,3.49,7.99]
 cart_item_prices.each do |x|
