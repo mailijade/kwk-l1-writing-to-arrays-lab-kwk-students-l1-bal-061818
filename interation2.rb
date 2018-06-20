d73dc7fcbdfb2b4092383fe86c036ec948ba604c
mailijade <mailijade23@gmail.com>
2018-06-20 19:58:10 +0000

    automatically backed up by learn

--- interation2.rb
+++ interation2.rb
@@ -3,10 +3,10 @@ cart_item_price = [12.50,19.99,3.49,7.99]
 cart_item_price.each do |x|
     total += x
 end
-if tattl , 25
+if tottl , 25
   total += 7.50
 else
   total=total
 end
 
-puts total
+puts totalr
