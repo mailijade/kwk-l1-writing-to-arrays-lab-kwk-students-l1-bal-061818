644511a03f71ff79b02398030d81b64e9d061377
mailijade <mailijade23@gmail.com>
2018-06-20 20:04:01 +0000

    automatically backed up by learn

--- interation2.rb
+++ interation2.rb
@@ -1,12 +1,16 @@
-total= 7.50
+ total= 0
+ cart_items_prices= [2.50,0.99,3.49,7.99]
+ cart_items_prices
+ total= 7.50
 cart_item_price = [12.50,19.99,3.49,7.99]
 cart_item_price.each do |x|
     total += x
 end
+
 if total < 25
   total += 7.50
 else
   total=total
 end
 
-puts total
+puts "#{total} is your total with shipping" 
